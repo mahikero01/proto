7f181d4dcfda17301a4cba17f6b0391a0920b460
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2014-12-09 05:36:10 +0800

    up

---
+++ test/home/rico/crewanalyzer/ImportFileTest.java
@@ -0,0 +1,14 @@
+package home.rico.crewanalyzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImportFileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
