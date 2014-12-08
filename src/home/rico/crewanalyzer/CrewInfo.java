ea7977b9fa3646394986fd81e975edb0c1d70609
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2014-12-08 21:13:39 +0800

    up

--- src/home/rico/crewanalyzer/CrewInfo.java
+++ src/home/rico/crewanalyzer/CrewInfo.java
@@ -1,5 +1,48 @@
 package home.rico.crewanalyzer;
 
 public class CrewInfo {
-
+	private short pin;
+	private String name;
+	private String rank;
+	private String datePromoted;
+	private String dateOfBirth;
+	private String startEmployment;
+	
+	public short getPin() {
+		return pin;
+	}
+	public void setPin(short pin) {
+		this.pin = pin;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getDatePromoted() {
+		return datePromoted;
+	}
+	public void setDatePromoted(String datePromoted) {
+		this.datePromoted = datePromoted;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getStartEmployment() {
+		return startEmployment;
+	}
+	public void setStartEmployment(String startEmployment) {
+		this.startEmployment = startEmployment;
+	}
+	
 }
