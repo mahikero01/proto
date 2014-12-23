309a8d77a2d413db43d9ef36e96cf1d55c832e77
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2014-12-23 23:41:17 +0800

    up

---
+++ src/home/rico/crewanalyzer/CrewStatRecord.java
@@ -0,0 +1,17 @@
+package home.rico.crewanalyzer;
+
+import java.util.Calendar;
+
+public class CrewStatRecord {
+	
+	private Calendar recordDate;
+
+	public Calendar getRecordDate() {
+		return recordDate;
+	}
+
+	public void setRecordDate(Calendar recordDate) {
+		this.recordDate = recordDate;
+	}
+
+}
